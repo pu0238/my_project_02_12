b5e2dff4e085e52559cf3db1bd42802a7f376992
pu0238 <pu0238@protonmail.com>
2024-12-02 18:12:18 +0000

    Added chats

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,22 +1,22 @@
 use std::cell::RefCell;
 
 thread_local! {
-    static MSG: RefCell<String> = RefCell::new(String::new());
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
 }
 
 
 #[ic_cdk::query]
-fn get_msg() -> String{
-    MSG.with(|msg| msg.borrow().clone())
+fn get_chat() -> Vec<String> {
+    CHAT.with(|chat| chat.borrow().clone())
 }
 
 #[ic_cdk::update]
-fn set_msg(new_msg: String) {
-    MSG.with(|msg| *msg.borrow_mut() = new_msg)
+fn add_msg(new_msg: String) {
+    CHAT.with(|chat| chat.borrow_mut().push(new_msg))
 }
 
-// new_msg: String
-// msg.borrow_mut(): &mut String
+// new_chat: String
+// chat.borrow_mut(): &mut String
 // &mut String != String ???
 // *(&mut String) == String
 
